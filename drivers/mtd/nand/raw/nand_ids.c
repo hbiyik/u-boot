f63b9228fa176bbe469e60e4868b493e128bc22b
boogie <boogiepop@gmx.com>
2026-07-18 23:30:51 +0200

    mtd: nand: raw: give a config option to disable samsung nand chips

--- drivers/mtd/nand/raw/nand_ids.c
+++ drivers/mtd/nand/raw/nand_ids.c
@@ -184,7 +184,9 @@ struct nand_manufacturer nand_manuf_ids[] = {
 #ifdef CONFIG_NAND_CHIP_TOSHIBA
 	{NAND_MFR_TOSHIBA, "Toshiba", &toshiba_nand_manuf_ops},
 #endif
+#ifdef CONFIG_NAND_CHIP_SAMSUNG
 	{NAND_MFR_SAMSUNG, "Samsung", &samsung_nand_manuf_ops},
+#endif
 	{NAND_MFR_FUJITSU, "Fujitsu"},
 	{NAND_MFR_NATIONAL, "National"},
 	{NAND_MFR_RENESAS, "Renesas"},
